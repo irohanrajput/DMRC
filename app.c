5ce3e5a33af25990bb4ad395834fa4a2f3c94d42
irohanrajput <itusharsingh27@gmail.com>
2024-01-12 19:07:36 +0530

    started

---
+++ app.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void displayInfo() {
+}
+
+void getUserInput(char *startStation, char *endStation) {
+}
+
+void calculateFare(char *startStation, char *endStation) {
+}
+
+void estimateTimeTaken(char *startStation, char *endStation) {
+}
+
+int main() {
+    char startStation[50];
+    char endStation[50];
+
+    displayInfo();
+    getUserInput(startStation, endStation);
+    calculateFare(startStation, endStation);
+    estimateTimeTaken(startStation, endStation);
+
+    return 0;
+}
